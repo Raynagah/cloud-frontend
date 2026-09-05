7e27240fe4a07d3b061d952cb0d5de86ae932822
nbello26 <nicolas.vicente1226@gmail.com>
2026-09-04 20:30:54 -0400

    feature - 2.2.0: Cambiar ruta interna de conexion

--- src/TokenSender.jsx
+++ src/TokenSender.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useMsal } from "@azure/msal-react";
 
 // Apuntamos directamente a tu microservicio local
-const MS_USUARIOS_URL = "http://3.208.93.5:8081/api/usuarios";
+const MS_USUARIOS_URL = "https://nku8zd8ok9.execute-api.us-east-1.amazonaws.com/desarrollo1/api/usuarios";
 
 export function TokenSender() {
     const { accounts } = useMsal();
