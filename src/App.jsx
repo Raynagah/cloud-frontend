8ff2e89f9cd3f3d15062aaa534e98e310222ba30
nbello26 <nicolas.vicente1226@gmail.com>
2026-09-05 16:59:38 -0400

    feature - 2.3.2: Arreglar bugs rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useIsAuthenticated } from "@azure/msal-react";
 
-// Importación de las futuras páginas
 import { LoginPage } from './pages/LoginPage';
 import { RegistroPage } from './pages/RegistroPage';
 import { DashboardPage } from './pages/DashboardPage';
@@ -11,21 +10,12 @@ function App() {
   const isAuthenticated = useIsAuthenticated();
 
   return (
-    <BrowserRouter>
+    // 👇 Añade el basename con el nombre exacto de tu stage en API Gateway
+    <BrowserRouter basename="/desarrollo">
       <Routes>
-        {/* Ruta pública de inicio de sesión */}
         <Route path="/" element={<LoginPage />} />
-
-        {/* Rutas protegidas: Requieren sesión activa en MSAL */}
-        <Route 
-          path="/registro" 
-          element={isAuthenticated ? <RegistroPage /> : <Navigate to="/" />} 
-        />
-        
-        <Route 
-          path="/dashboard" 
-          element={isAuthenticated ? <DashboardPage /> : <Navigate to="/" />} 
-        />
+        <Route path="/registro" element={isAuthenticated ? <RegistroPage /> : <Navigate to="/" />} />
+        <Route path="/dashboard" element={isAuthenticated ? <DashboardPage /> : <Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
